9b1743d2933c71866cb3f03b379817a6b63de224
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-04-09 18:07:48 -0300

    dayOne

---
+++ src/introducaoCursoJava/JavaDayOne.java
@@ -0,0 +1,7 @@
+package introducaoCursoJava;
+
+public class JavaDayOne {
+    public static void main(String[] args) {
+        System.out.println("meu Deus, meu Deus, eu sou um hacker");
+    }
+}
