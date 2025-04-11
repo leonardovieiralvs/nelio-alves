7f571403034cba36a1f0aaf074a5bdd324fb9fe5
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-04-11 09:58:14 -0300

    Switch

--- src/introducaoCursoJava/Test.java
+++ src/introducaoCursoJava/Test.java
@@ -11,17 +11,19 @@ public class Test {
             case 1:
                 System.out.println("Segunda-feira"); break;
             case 2:
-                System.out.println("Terça-feira");
+                System.out.println("Terça-feira"); break;
             case 3:
-                System.out.println("Quarta-feira");
+                System.out.println("Quarta-feira"); break;
             case 4:
-                System.out.println("Quinta-feira");
+                System.out.println("Quinta-feira"); break;
             case 5:
-                System.out.println("Sexta-feira");
+                System.out.println("Sexta-feira"); break;
             case 6:
-                System.out.println("Sábado");
+                System.out.println("Sábado"); break;
+            case 7:
+                System.out.println("Domingo"); break;
             default:
-                System.out.println("Domingo");
+                System.out.println("Dia Invalido"); break;
         }
     }
 }
