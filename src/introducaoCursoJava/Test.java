c8db94c3bf07b8a75cd80c56163f7d414898bae7
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-04-11 20:40:12 -0300

    FOR

--- src/introducaoCursoJava/Test.java
+++ src/introducaoCursoJava/Test.java
@@ -4,33 +4,24 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        int num = 0;
-        int soma = 0;
-        int alcool = 0;
-        int gasolina = 0;
-        int diesel = 0;
         Scanner input = new Scanner(System.in);
-        System.out.println("[1] Alcool");
-        System.out.println("[2] Gasolina");
-        System.out.println("[3] Diesel");
-        System.out.println("[4] Fim");
-        while (num != 4) {
-            System.out.println("#### Escolha o tipo do combustivel: ");
-            num = input.nextInt();
-            if (num == 1) {
-                alcool++;
-            } else if (num == 2) {
-                gasolina++;
-            } else if (num == 3) {
-                diesel++;
-            } else if (num == 4){
-                System.out.println("MUITO OBRIGADO");
+
+        int in = 0;
+        int out = 0;
+
+        System.out.print("Digite quantidade de deseja repetir o LOOP: ");
+        int loop = input.nextInt();
+        for (int i = 0; i < loop; i++) {
+            System.out.print("Digite o desejado numero: ");
+            int num = input.nextInt();
+            if (num >= 10 && num <= 20) {
+                in++;
             } else {
-                System.out.println("Numero invalido!");
+                out++;
             }
         }
-        System.out.println("Alcool: "+alcool);
-        System.out.println("Gasolina: "+gasolina);
-        System.out.println("Diesel: "+diesel);
+        System.out.println(in+" in");
+        System.out.println(out+" out");
+        input.close();
     }
 }
