1fd724213eebc9f51b2d07692288593f65307a35
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-04-11 12:09:27 -0300

    Ex. While

--- src/introducaoCursoJava/Test.java
+++ src/introducaoCursoJava/Test.java
@@ -4,26 +4,33 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
+        int num = 0;
+        int soma = 0;
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
         Scanner input = new Scanner(System.in);
-        System.out.println("Digite o numero que corresponde o dia da semana: ");
-        int dia = input.nextInt();
-        switch (dia) {
-            case 1:
-                System.out.println("Segunda-feira"); break;
-            case 2:
-                System.out.println("Terça-feira"); break;
-            case 3:
-                System.out.println("Quarta-feira"); break;
-            case 4:
-                System.out.println("Quinta-feira"); break;
-            case 5:
-                System.out.println("Sexta-feira"); break;
-            case 6:
-                System.out.println("Sábado"); break;
-            case 7:
-                System.out.println("Domingo"); break;
-            default:
-                System.out.println("Dia Invalido"); break;
+        System.out.println("[1] Alcool");
+        System.out.println("[2] Gasolina");
+        System.out.println("[3] Diesel");
+        System.out.println("[4] Fim");
+        while (num != 4) {
+            System.out.println("#### Escolha o tipo do combustivel: ");
+            num = input.nextInt();
+            if (num == 1) {
+                alcool++;
+            } else if (num == 2) {
+                gasolina++;
+            } else if (num == 3) {
+                diesel++;
+            } else if (num == 4){
+                System.out.println("MUITO OBRIGADO");
+            } else {
+                System.out.println("Numero invalido!");
+            }
         }
+        System.out.println("Alcool: "+alcool);
+        System.out.println("Gasolina: "+gasolina);
+        System.out.println("Diesel: "+diesel);
     }
 }
