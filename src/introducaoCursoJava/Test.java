21fbbd950d63d02824b6b83d5e363d6943c9f454
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-04-11 09:57:17 -0300

    Switch

--- src/introducaoCursoJava/Test.java
+++ src/introducaoCursoJava/Test.java
@@ -4,16 +4,24 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        double conta = 50;
-        int minuto;
-
         Scanner input = new Scanner(System.in);
-        System.out.println("Minutos usados no plano telefonico: ");
-        minuto = input.nextInt();
-        if (minuto > 100) {
-            conta += (minuto - 100) * 2;
+        System.out.println("Digite o numero que corresponde o dia da semana: ");
+        int dia = input.nextInt();
+        switch (dia) {
+            case 1:
+                System.out.println("Segunda-feira"); break;
+            case 2:
+                System.out.println("Terça-feira");
+            case 3:
+                System.out.println("Quarta-feira");
+            case 4:
+                System.out.println("Quinta-feira");
+            case 5:
+                System.out.println("Sexta-feira");
+            case 6:
+                System.out.println("Sábado");
+            default:
+                System.out.println("Domingo");
         }
-        System.out.printf("Valor a pagar: R$ %.2f%n", + conta);
-        input.close();
     }
 }
