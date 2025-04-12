9cf5a2df55f29f84b61c56002457019c0375770b
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-04-12 00:37:45 -0300

    Update Test.java

--- src/introducaoCursoJava/Test.java
+++ src/introducaoCursoJava/Test.java
@@ -5,23 +5,16 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        int in = 0;
-        int out = 0;
-
+        double soma = 0;
         System.out.print("Digite quantidade de deseja repetir o LOOP: ");
         int loop = input.nextInt();
+
         for (int i = 0; i < loop; i++) {
             System.out.print("Digite o desejado numero: ");
-            int num = input.nextInt();
-            if (num >= 10 && num <= 20) {
-                in++;
-            } else {
-                out++;
-            }
+            double num = input.nextDouble();
+            soma += (num) / loop;
         }
-        System.out.println(in+" in");
-        System.out.println(out+" out");
+        System.out.printf("%.2f%n", soma);
         input.close();
     }
 }
