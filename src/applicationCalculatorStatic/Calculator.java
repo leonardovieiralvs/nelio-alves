c82fc54bfffa5c417b9a82f62466455391f59fcd
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-04-19 07:20:35 -0300

    CurrencyConverter

--- src/applicationCalculatorStatic/Calculator.java
+++ src/applicationCalculatorStatic/Calculator.java
@@ -1,7 +1,8 @@
 package applicationCalculatorStatic;
 
 public class Calculator {
-    public static double PI = 3.14159;
+    public final static double PI = 3.14159;
+    public final static double PORCENTAGE = 6.0;
 
     public static double circumference(double radius) {
         return 2 * PI * radius;
@@ -9,5 +10,11 @@ public class Calculator {
 
     public static double volume(double radius) {
         return 4.0 * PI * radius * radius * radius / 3.0;
+    }
+
+    public static double currencyConverter(double price, double change) {
+//        double result = price * change * PORCENTAGE / 100;
+//        return price * change + result;
+        return price * change * (1 + PORCENTAGE / 100);
     }
 }
---
+++ src/applicationCalculatorStatic/CurrencyConverter.java
@@ -0,0 +1,19 @@
+package applicationCalculatorStatic;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CurrencyConverter {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is the dollar price? ");
+        double price = sc.nextDouble();
+        System.out.print("How many dollar will be bought: ");
+        double change = sc.nextDouble();
+        double v = Calculator.currencyConverter(price, change);
+        System.out.printf("Amount to be paid in reais = %.2f%n", v);
+
+        sc.close();
+    }
+}
