855c39d41b6a9aab850268131367f3b3543f4f6d
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-05-11 16:57:13 -0300

    Matriz

--- src/introducaoCursoJava/matrizes/Matrizes3x3.java
+++ src/introducaoCursoJava/matrizes/Matrizes3x3.java
@@ -3,7 +3,7 @@ package introducaoCursoJava.matrizes;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Matrizes {
+public class Matrizes3x3 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
