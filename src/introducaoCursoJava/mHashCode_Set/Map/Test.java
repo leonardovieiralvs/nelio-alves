1b74ec31712028b08a3ae2d021b5f593fe2d9530
leonardovieiralvs <leonardo_vieira199@hotmail.com>
2025-07-03 21:53:48 -0300

    Map<K,V>

---
+++ src/introducaoCursoJava/mHashCode_Set/Map/Test.java
@@ -0,0 +1,45 @@
+package introducaoCursoJava.mHashCode_Set.Map;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter file full path: ");
+        String path = sc.next();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            Map<String, Integer> votes = new HashMap<>();
+            String file = br.readLine();
+
+            while (file != null) {
+
+                String[] fields = file.split(",");
+                String name = fields[0];
+                int count = Integer.parseInt(fields[1]);
+
+                if (votes.containsKey(name)) {
+                    int votesSoFar = votes.get(name);
+                    votes.put(name, count + votesSoFar);
+                } else {
+                    votes.put(name, count);
+                }
+
+                file = br.readLine();
+
+            }
+
+            for (String key : votes.keySet()) {
+                System.out.println(key + ": " + votes.get(key));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
